b4e585f9f9ce8e691375bb2721e6c28d26f8945a
ibochidev <119848938+ibochidev@users.noreply.github.com>
2023-08-08 10:29:39 +0500

    Create config.py
    update Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import ParseMode
+import logging
+from sql import Database
+
+ADMINS = ['6032988740','6041962881']
+BOT_TOKEN = '6489036760:AAFCnWx_keKdEVi7Zcl3GlVfs_OqxXWW9Zc'
+
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+db = Database(path_to_db="main.db")
+logging.basicConfig(level=logging.INFO)
