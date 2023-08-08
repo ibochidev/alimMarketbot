795c33ef1d2e1c85413c508e12e71d6fa2e488c6
ibochidev <119848938+ibochidev@users.noreply.github.com>
2023-08-08 11:09:17 +0500

    Create text.py

---
+++ text.py
@@ -0,0 +1,11 @@
+start_handler = """<b>Assalomu aleykum xurmatli mijoz. 
+Siz Alim Market bot orqali istalgan turdagi mahsulotlarimizni online ravishda sotib olishingiz mumkin</b>"""
+
+product_name_text ="""Siz buyurtma qilmoqchi bolgan, Maxsulot nomini kiriting:"""
+product_size_text = """Siz buyurtma qilmoqchi bolgan, Maxsulot razmerini kiriting:"""
+product_description_text = """Siz buyurtma qilmoqchi bolgan, Maxsulot tavsifini kiriting:"""""
+phone_number_text = """Telefon raqamingizni kiriting:"""
+product_photo_text = """Siz buyurtma qilmoqchi bolgan, Maxsulotning taxminiy rasmini yuboring::"""
+check_product_text = """Buyurtma adminga yuborilsinmi"""
+
+send_check = """Buyurtma yuborildi siz bilan 5 daqiqada bog`lanamiz"""
