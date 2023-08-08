d41160b2b9de8212767a2d496462476ce6d96097
ibochidev <119848938+ibochidev@users.noreply.github.com>
2023-08-08 11:50:24 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import logging
 from sql import database 
 
 ADMINS = ['6041962881']
-BOT_TOKEN = '6489036760:AAFCnWx_keKdEVi7Zcl3GlVfs_OqxXWW9Zc'
+BOT_TOKEN = '6489036760:AAHdm1c-06ExF8n2FmA_dzWs9mWtyUcTDqE'
 
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 storage = MemoryStorage()
